0535efc617cdb7639a80e5e238ce5d7d6183ba64
om-666 <104663400+om-666@users.noreply.github.com>
2022-12-01 21:19:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,64 @@
+function playsound()
+{
+  var audio=document.getElementById("audio");
+ 
+
+  audio.play();
+}
+function playsound2()
+{
+  var audio1=document.getElementById("audio1");
+ 
+
+  audio1.play();
+}
+function playsound3()
+{
+  var audio2=document.getElementById("audio2");
+ 
+
+  audio2.play();
+}
+function playsound4()
+{
+  var audio3=document.getElementById("audio3");
+ 
+
+  audio3.play();
+}
+function playsound5()
+{
+  var audio4=document.getElementById("audio4");
+ 
+
+  audio4.play();
+}
+function playsound6()
+{
+  var audio5=document.getElementById("audio5");
+ 
+
+  audio5.play();
+}
+ 
+
+
+
+
+
+
+
+
+var swiper = new Swiper(".home", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    
+  });
